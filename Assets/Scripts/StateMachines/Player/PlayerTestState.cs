439be658189dad7a5e599a66d42bfb75d9daa935
vladstheawesome <vmabhena@gmail.com>
2022-06-12 18:15:52 +0100

    Movement Input

--- Assets/Scripts/StateMachines/Player/PlayerTestState.cs
+++ Assets/Scripts/StateMachines/Player/PlayerTestState.cs
@@ -4,26 +4,23 @@ using UnityEngine;
 
 public class PlayerTestState : PlayerBaseState
 {
-    private float timer = 0f;
     public PlayerTestState(PlayerStateMachine stateMachine) : base(stateMachine) { }
 
     public override void Enter()
     {
-        stateMachine.InputReader.JumpEvent += OnJump;
+        
     }
     public override void Tick(float deltaTime)
     {
-        timer += deltaTime;
-        Debug.Log(timer);        
+        Vector3 movement = new Vector3();
+        movement.x = stateMachine.InputReader.MovementValue.x;
+        movement.y = 0;
+        movement.z = stateMachine.InputReader.MovementValue.y;
+        stateMachine.transform.Translate(movement * deltaTime);       
     }
 
     public override void Exit()
     {
-        stateMachine.InputReader.JumpEvent -= Enter;
-    }
-
-    public void OnJump()
-    {
-        stateMachine.SwitchState(new PlayerTestState(stateMachine));
+        
     }
 }
